ef72e3571c0d2596e8face27f1bcd38142d4f4f7
tooru <tooru.utada@systemi.co.jp>
2024-08-21 10:24:28 +0900

    test2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/test1',
+        destination: 'https://acme.com/blog',
+      },
+    ]
+  },
+
 }
+
